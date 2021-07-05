c2806f23ec5259f4bcbcc2f4525a44a1ff3bb880
chris <cseiter@gmail.com>
2021-07-05 14:52:51 -0400

    batch_badge_creator module passes test

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,13 @@ def badge_maker(name)
     "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(array_names)
+    badge_array = []
+    array_names.each { |ind_name| badge_array << "Hello, my name is #{ind_name}."}
+    return badge_array
+end
+
 
 badge_maker("sam")
+
+batch_badge_creator(["sam","dean","cas","bobby"])
