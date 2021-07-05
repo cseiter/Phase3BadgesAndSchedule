71c5fbd9207ace2db7141df82af86d019c51a69f
chris <cseiter@gmail.com>
2021-07-05 15:04:50 -0400

    assign_rooms module passed test

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,15 @@ def batch_badge_creator(array_names)
     return badge_array
 end
 
+def assign_rooms(array_names)
+    room_array = []
+    array_names.map.with_index(1) { |ind_name,room_num| room_array << "Hello, #{ind_name}! You'll be assigned to room #{room_num}!"}
+    return room_array
+end
 
 badge_maker("sam")
 
-batch_badge_creator(["sam","dean","cas","bobby"])
+array_names = ["sam","dean","cas","bobby"]
+
+batch_badge_creator(array_names)
+assign_rooms(array_names)
